0203ed272f5cadd4d5cf688368ecaf39029f512d
dieudonne-fianko <dieudonne.fianko1@gmail.com>
2025-07-10 23:46:40 +0200

    Update dashboard.php

--- visualisation/dashboard.php
+++ visualisation/dashboard.php
@@ -83,75 +83,75 @@ if (isset($_GET['indicateurs'])) {
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="keywords" content="déserts médicaux, surcharge des urgences, indicateurs santé, France, santé publique, tableau de bord, visualisation données">
-  <meta name="description" content="Comprenez les déserts médicaux et la surcharge des urgences grâce à un tableau de bord interactif. Comparez les régions de France via des indicateurs clairs et visuels.">
-  <meta name="author" content="Dieu-Donné FIANKO,Sayeh FATEN,Zoughbi MERIEM">
-  <title>Tableau de bord -- Déserts médicaux et Surcharge des urgences</title>
-  <link rel="stylesheet" href="../styles/style_dashboard.css" />
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <link rel="icon" href="../images/favicon.png">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
-</head>
-<body>
-  <header>
-      <div class="logo-zone">
-          <nav>
-              <a href="../index.html#accueil">Accueil</a>
-              <a href="../index.html#apropos">A propos</a>
-              <a href="../index.html#data">Données & Méthodes</a>
-              <a href="../index.html#dashboard">Visualisations</a>
-              <a href="../index.html#contact">Contact</a>
-          </nav>
-      </div>
-  </header>
-
-  <main class="dashboard">
-    <aside class="sidebar">
-      <div class="toolbar">
-        <div class="checkbox-wrapper">
-          <label><input type="checkbox" id="doubleInd" /> Affichage double indicateur</label>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="déserts médicaux, surcharge des urgences, indicateurs santé, France, santé publique, tableau de bord, visualisation données">
+    <meta name="description" content="Comprenez les déserts médicaux et la surcharge des urgences grâce à un tableau de bord interactif. Comparez les régions de France via des indicateurs clairs et visuels.">
+    <meta name="author" content="Dieu-Donné FIANKO,Sayeh FATEN,Zoughbi MERIEM">
+    <title>Tableau de bord -- Déserts médicaux et Surcharge des urgences</title>
+    <link rel="stylesheet" href="../styles/style_dashboard.css" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="icon" href="../images/favicon.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+  </head>
+  <body>
+    <header>
+        <div class="logo-zone">
+            <nav>
+                <a href="../index.html#accueil">Accueil</a>
+                <a href="../index.html#apropos">A propos</a>
+                <a href="../index.html#data">Données & Méthodes</a>
+                <a href="../index.html#dashboard">Visualisations</a>
+                <a href="../index.html#contact">Contact</a>
+            </nav>
         </div>
-        <select id="indicateur1" class="input"></select>
-        <select id="indicateur2" class="input" style="display: none;"></select>
-        <div class="toolbar-buttons">
-          <button id="refreshBtn" class="btn">Actualiser</button>
-          <button id="downloadChart" class="btn">Télécharger en PNG</button>
-          <button id="downloadXLSX" class="btn">Télécharger en Excel</button>
-        </div>
-      </div>
-    </aside>
+    </header>
 
-    <section class="charts">
-      <div class="chart-card">
-        <canvas id="barChart"></canvas>
-      </div>
-      <div class="chart-card">
-        <div id="gridContainer" class="grid-13"></div>
-        <div class="legend">
-          <div><div class="color-box" style="background:green"></div>Régions exemptes</div>
-          <div><div class="color-box" style="background:orange"></div>Régions touchées par la désertification médicale</div>
-          <div><div class="color-box" style="background:purple"></div>Régions touchées par la surcharge des urgences</div>
-          <div><div class="color-box" style="background:red"></div>Régions touchées par la désertification médicale et la surcharge des urgences</div>
+    <main class="dashboard">
+      <aside class="sidebar">
+        <div class="toolbar">
+          <div class="checkbox-wrapper">
+            <label><input type="checkbox" id="doubleInd" /> Affichage double indicateur</label>
+          </div>
+          <select id="indicateur1" class="input"></select>
+          <select id="indicateur2" class="input" style="display: none;"></select>
+          <div class="toolbar-buttons">
+            <button id="refreshBtn" class="btn">Actualiser</button>
+            <button id="downloadChart" class="btn">Télécharger en PNG</button>
+            <button id="downloadXLSX" class="btn">Télécharger en Excel</button>
+          </div>
         </div>
-      </div>
-    </section>
-  </main>
+      </aside>
 
-  <footer id="pied-de-page">
-      <div class="footer-logos">
-          <img src="../images/uspn.png" alt="Logo Université Sorbonne Paris Nord">
-          <img src="../images/smhb.png" alt="Logo UFR Santé Médecine Biologie Humaine">
-      </div>
-      <p>
+      <section class="charts">
+        <div class="chart-card">
+          <canvas id="barChart"></canvas>
+        </div>
+        <div class="chart-card">
+          <div id="gridContainer" class="grid-13"></div>
+          <div class="legend">
+            <div><div class="color-box" style="background:green"></div>Régions exemptes</div>
+            <div><div class="color-box" style="background:orange"></div>Régions touchées par la désertification médicale</div>
+            <div><div class="color-box" style="background:purple"></div>Régions touchées par la surcharge des urgences</div>
+            <div><div class="color-box" style="background:red"></div>Régions touchées par la désertification médicale et la surcharge des urgences</div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer id="pied-de-page">
+        <div class="footer-logos">
+            <img src="../images/uspn.png" alt="Logo Université Sorbonne Paris Nord">
+            <img src="../images/smhb.png" alt="Logo UFR Santé Médecine Biologie Humaine">
+        </div>
+        <p>
           Projet tutoré – Master 1 Santé Publique, parcours Informatique Biomédicale<br>
           Université Sorbonne Paris Nord – UFR SMBH | Année académique 2024–2025
-      </p>
-      <p class="footer-credits">© <span id="annee-courante"></span> – Tous droits réservés</p>
-  </footer>
-  <script src="../scripts/script_dashboard.js"></script>
-  <script src="../scripts/script_index.js"></script>
-</body>
+        </p>
+        <p class="footer-credits">© <span id="annee-courante"></span> – Tous droits réservés</p>
+    </footer>
+    <script src="../scripts/script_dashboard.js"></script>
+    <script src="../scripts/script_index.js"></script>
+  </body>
 </html>
